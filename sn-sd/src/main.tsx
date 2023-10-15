b69952bee0f9d1660dcffc5cb11ac7319ff03588
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-10-15 19:00:32 +0530

    Minor change on mina.tsx file

--- sn-sd/src/main.tsx
+++ sn-sd/src/main.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+// import React from 'react'
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
+import "./index.css";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
