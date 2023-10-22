d59037536b8439edb5f83f9dceb0d22009169c71
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-10-22 13:18:15 +0530

    ArticlesList Item page has added to the project for each articles with image, title and necessary details

---
+++ sn-sd/src/pages/articles/ArticleListItem.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable */
+import { useArticlesState } from "../../context/articles/context";
+import { Link } from "react-router-dom";
+
+export default function ArticleListItems() {
+  let state: any = useArticlesState(); // we define this for type checking
+  // const dispatch = useArticlesDispatch();
+  console.log('state', state)
+  if(!state) return null;
+  const { articles, isLoading, isError, errorMessage } = state;
+  console.log('fetched articles',articles);
+
+  if (articles.length === 0 && isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>{errorMessage}</span>;
+  }
+  // filter the articles by time and date..start from the latest
+  articles.sort((a: any, b: any) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  }
+  );
+  // // This check is to validate ErrorBoundary Implementation. Remove this before milestone submission
+  // if (projects.length === 0) {
+  //   throw Error("Error!!!");
+  // }
+
+  return (
+    <>
+      {articles.map((article: any) => (
+        <Link key={article.id} to={`${article.id}`}>
+          <div className="flex flex-row bg-white rounded-md shadow-sm">
+            <div style={{ flex: 1 }}>
+              <div className="flex flex-row justify-between items-center px-4 py-3">
+                <h3 className="text-lg font-bold text-gray-800">
+                  {article.sport.name}
+                </h3>
+              </div>
+              <div className="px-4 py-3">
+                <h2 className="text-xl text-left font-bold text-gray-800  line-clamp-1">
+                  {article.title}
+                </h2>
+                <p className="text-base text-start text-gray-600 line-clamp-2">
+                  {article.summary}
+                </p>
+              </div>
+              <div className="flex flex-row justify-between items-center px-4 py-3">
+                <p className="text-sm font-medium text-gray-600">
+                  {article.date}
+                </p>
+                <a className="text-sm font-medium text-gray-600">
+                  Read More...
+                </a>
+              </div>
+            </div>
+            <div className="flex-shrink-0 flex justify-center items-center">
+              <img
+                className="object-cover h-48 w-48 rounded-md"
+                src={article.thumbnail}
+                alt=""
+              />
+            </div>
+          </div>
+        </Link>
+      ))}
+    </>
+  );
+}
